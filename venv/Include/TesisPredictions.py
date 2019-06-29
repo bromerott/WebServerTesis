ccad33d345db34a68458ede59cd5be62505bf945
Bruno Romero <bromerott@gmail.com>
2019-06-28 21:14:58 -0500

    cambios de autenticacion

--- venv/Include/TesisPredictions.py
+++ venv/Include/TesisPredictions.py
@@ -2,26 +2,43 @@
 from flask import Flask, jsonify, abort
 from flask import make_response
 from flask import request
-import predictions
+import predictionController
 import time
 import atexit
 import pandas as pd
+import authenticationController
 
 app = Flask(__name__)
 
-contPredictions = predictions.predictionController()
-
-#Metodo de retorno de las Asignaciones
+contPredictions = predictionController.predictionController()
+contAuthentication = authenticationController.authenticationController()
+#GET Request to return today's predictions
 @app.route('/TesisPredictions/getPredictions', methods=['GET'])
 def get_Asignaciones():
     contPredictions.dailyPrediction()
     return jsonify({'Predictions':contPredictions.getPredictions()})
-    
-#Metodo de mapeo de errores
+
+#POST Request to return today's predictions
+@app.route('/TesisPredictions/authenticate', methods=['POST'])
+def authenticate():
+    reqData = request.get_json()
+    email = reqData['email']
+    password = reqData['password']
+    return jsonify(contAuthentication.authenticate(email,password))
+
+#POST Request to register new User
+@app.route('/TesisPredictions/registerUser', methods=['POST'])
+def register():
+    reqData = request.get_json()
+    email = reqData['email']
+    password = reqData['password']
+    contAuthentication.registerUser(email,password)
+    return jsonify({'User': 'Registered'})
+
+#Error Mapping
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-#Creacion de tarea daemon que ejecute el algoritmo de asignacion de vuelos cada 15 segundos
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
